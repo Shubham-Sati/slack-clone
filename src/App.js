2bbbcee4ac5159a3a5543dd0b910cf021b4f785a
Shubham Sati <shubham.sati8b@gmail.com>
2022-07-22 13:10:46 +0530

    Header Creted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from "react";
+import './App.css';
 
+import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <p>this is starting point</p>
       {/* Header */}
-      {/* sideBar */}
-      {/* React-Router -> chat screen */}
+      <Header />
+
+      <div className="app__body">
+          {/* sideBar */}
+          <Sidebar />
+
+
+          {/* React-Router -> chat screen */}
+      </div>
 
     </div>
   );
